86f67f5d4f24cba23b0368c3ff559e181c7889f8
AdityaRws <90436742+AdityaRws@users.noreply.github.com>
2021-11-15 09:38:20 +0700

    Add files via upload

---
+++ AdityaRamadhanWahyuSantoso_055_Permasalahan 25.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+void garis();
+main()
+{
+	garis();
+	printf("NO NIM NAMA NILAI\n");
+	garis();
+	getch();
+}
+void garis()
+{
+	printf("==========================\n");
+}
